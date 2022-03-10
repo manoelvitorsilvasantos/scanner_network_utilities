cb1da2e7be832e7ebb59e3465a57e594f595b46a
Mvictor <mvictordossantos.limite@gmail.com>
2022-03-10 00:49:04 -0300

    Update port.py

--- port.py
+++ port.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 from sys import argv
 import socket
 
+#criar um soquete
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #função para escamento de porta
@@ -13,17 +16,14 @@ def scanner_port(host, port):
     else:
         print('%d | is CLOSED' %(port))
 
-host = argv[1]
-port = int(argv[3])
-
-#res = my_port.split('-p', -1)
-#port = int(res[1])
-
+host = argv[1] #argumento pega o host
+port = int(argv[3]) #argumento pega a porta
 
 if host == None and port == None:
     print('python3 ./port [target] -p [port]\n')
 else:
     scanner_port(host, port)
     sock.close()
+
 
     
